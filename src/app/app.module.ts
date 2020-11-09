1f9dc437f59b6054136f027bc2d30d0e0dcfc4b8
Guy Krief <guy.krief6@gmail.com>
2020-11-09 18:30:44 +0200

    bootstrap added to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginScreenComponent } from './login-screen/login-screen.component';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import { FooterComponent } from './footer/footer.component';
+import { GameScreenComponent } from './game-screen/game-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginScreenComponent
+    LoginScreenComponent,
+    FooterComponent,
+    GameScreenComponent
   ],
   imports: [
     BrowserModule,
     StoreModule.forRoot({}, {}),
-    NgbModule
+    NgbModule,
+    MDBBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
