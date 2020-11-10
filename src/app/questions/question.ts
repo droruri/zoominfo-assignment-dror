2b62f7ba3d51e6e08e036fd5ccf23b6e70662cb7
Guy Krief <guy.krief6@gmail.com>
2020-11-11 01:02:48 +0200

    data from server creates Question entities

--- src/app/questions/question.ts
+++ src/app/questions/question.ts
@@ -2,4 +2,10 @@ export class Question {
   question: string;
   correctAnswer: string;
   incorrectAnswers: string[];
+
+  constructor(question: string, correctAnswer: string, incorrectAnswers: string[]) {
+    this.question = question;
+    this.correctAnswer = correctAnswer;
+    this.incorrectAnswers = incorrectAnswers;
+  }
 }
--- src/app/questions/questions.service.ts
+++ src/app/questions/questions.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {catchError, map} from 'rxjs/operators';
 import {Question} from './question';
+import {Store} from '@ngrx/store';
+import {CORRECT_HEADER, INCORRECT_HEADER, QUESTION_HEADER} from '../shared/global';
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +18,14 @@ export class QuestionsService {
 
   public getQuestions(): Observable<Question[]> {
     return this.http.get<any>(this.url).pipe(
-      map(response => response.results),
+      map(response => {
+        return response.results.map(questionData => this.createQuestion(questionData));
+      }),
       catchError(null)
     );
+  }
+
+  private createQuestion(questionData: any): Question {
+    return new Question(questionData[QUESTION_HEADER], questionData[CORRECT_HEADER], questionData[INCORRECT_HEADER]);
   }
 }
