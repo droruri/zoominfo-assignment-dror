86171af56eb47e7feefafc6e6e95e16a2a495ac3
Guy Krief <guy.krief6@gmail.com>
2020-11-11 01:02:08 +0200

    added global consts file

---
+++ src/app/shared/global.ts
@@ -0,0 +1,4 @@
+export const NUM_OF_QUESTIONS = 10;
+export const QUESTION_HEADER = 'question';
+export const CORRECT_HEADER = 'correct_answer';
+export const INCORRECT_HEADER = 'incorrect_answers';
