fb899181d87939587a337b50acdbf752644d2f65
Guy Krief <guy.krief6@gmail.com>
2020-11-11 00:58:30 +0200

    game screen continued design

--- src/app/game-screen/game-screen.component.ts
+++ src/app/game-screen/game-screen.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {QuestionsService} from '../questions/questions.service';
 import {Question} from '../questions/question';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-game-screen',
@@ -9,15 +10,38 @@ import {Question} from '../questions/question';
 })
 export class GameScreenComponent implements OnInit {
 
-  gameQuestions: Question[];
+  gameQuestions$: Question[];
+  questionNumberCounter = 1;
+  currentQuestion: Question;
+  answerChosen: string;
 
   constructor(private questionsService: QuestionsService) { }
 
   ngOnInit(): void {
-    this.questionsService.getQuestions().subscribe(data => {
-      this.gameQuestions = data;
-      console.log(this.gameQuestions);
+    this.questionsService.getQuestions().pipe(first()).subscribe(data => { // TODO: validate first is used good
+      this.gameQuestions$ = data;
+      console.log(this.gameQuestions$);
+      this.currentQuestion = this.gameQuestions$[0];
     });
   }
 
+  getCurrentQuestion(): string {
+    return this.gameQuestions$[this.questionNumberCounter - 1].question;
+  }
+
+  getCurrentCorrectAnswer(): string {
+    return this.gameQuestions$[this.questionNumberCounter - 1].correctAnswer;
+  }
+
+  onAnswerSubmitted(): void {
+    if (this.getCurrentCorrectAnswer() === this.answerChosen) {
+
+    }
+    this.questionNumberCounter++;
+    this.currentQuestion = this.gameQuestions$[this.questionNumberCounter - 1];
+  }
+
+  getChosenAnswer($event: string): void {
+    this.answerChosen = $event;
+  }
 }
