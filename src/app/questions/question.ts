d2a79adf5428fb91bd07c23a78358de75a910e6f
Guy Krief <guy.krief6@gmail.com>
2020-11-11 14:17:02 +0200

    new field at question class, to store whether the user answered right or wrong.

--- src/app/questions/question.ts
+++ src/app/questions/question.ts
@@ -2,6 +2,7 @@ export class Question {
   question: string;
   correctAnswer: string;
   incorrectAnswers: string[];
+  isCorrectAnswer = null;
 
   constructor(question: string, correctAnswer: string, incorrectAnswers: string[]) {
     this.question = question;
