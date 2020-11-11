9a3f1048c4bdb9b6ca725969ff6cedb042b79f70
Guy Krief <guy.krief6@gmail.com>
2020-11-11 10:56:44 +0200

    alphanumeric directive implemented

--- src/app/core/directives/alphanumeric.directive.ts
+++ src/app/core/directives/alphanumeric.directive.ts
@@ -7,6 +7,11 @@ export class AlphanumericDirective {
 
   constructor() { }
 
-  // @HostListener('keyup', ['$event']) onKeyUp(e): boolean {
-  // }
+  @HostListener('keypress', ['$event']) onKeyPress(event: KeyboardEvent): void {
+    if ((event.key >= '0' && event.key <= '9') || (event.key >= 'A' && event.key <= 'z')) {
+      return;
+    }
+
+    event.preventDefault();
+  }
 }
