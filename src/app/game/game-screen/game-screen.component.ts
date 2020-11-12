111db0a7b3b98d57e43be76baccd5159c477fe16
Guy Krief <guy.krief6@gmail.com>
2020-11-12 02:54:14 +0200

    using states and adding interaction with option-card.

--- src/app/game/game-screen/game-screen.component.ts
+++ src/app/game/game-screen/game-screen.component.ts
@@ -1,7 +1,10 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {QuestionsService} from '../../questions/questions.service';
+import {Component, OnChanges, OnDestroy, OnInit, QueryList, SimpleChanges, ViewChildren} from '@angular/core';
+import {GameService} from '../../questions/game.service';
 import {Question} from '../../questions/question';
 import { first } from 'rxjs/operators';
+import {GameData} from '../game-data';
+import {ActivatedRoute} from '@angular/router';
+import {OptionCardComponent} from '../../option-card/option-card.component';
 
 @Component({
   selector: 'app-game-screen',
@@ -9,26 +12,29 @@ import { first } from 'rxjs/operators';
   styleUrls: ['./game-screen.component.css']
 })
 export class GameScreenComponent implements OnInit {
+  @ViewChildren(OptionCardComponent) options: QueryList<OptionCardComponent>;
 
+  gameData: GameData;
   gameQuestions$: Question[];
   questionNumberCounter = 1;
   currentQuestion: Question;
   answerChosen: string;
 
-  constructor(private questionsService: QuestionsService) { }
+  constructor(private gameService: GameService,
+              private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
-    this.questionsService.getQuestions().pipe(first()).subscribe(data => { // TODO: validate first is used good
+    this.gameService.getQuestions().pipe(first()).subscribe(data => { // TODO: validate first is used good
       this.gameQuestions$ = data;
-      console.log(this.gameQuestions$);
+      this.gameData = new GameData(this.activatedRoute.snapshot.paramMap.get('username'), this.gameQuestions$);
+      this.gameService.setStartGameData(this.gameData);
       this.currentQuestion = this.gameQuestions$[0];
     });
   }
 
   getCurrentQuestion(): string {
     return this.gameQuestions$[this.questionNumberCounter - 1].question;
-    // return this.gameQuestions$[this.questionNumberCounter - 1].question.replace(/&quot;/g, '\"');
-    // return this.gameQuestions$[this.questionNumberCounter - 1].question.replace(/&#039; /g, '\'');
   }
 
   getCurrentCorrectAnswer(): string {
@@ -37,13 +43,21 @@ export class GameScreenComponent implements OnInit {
 
   onAnswerSubmitted(): void {
     if (this.getCurrentCorrectAnswer() === this.answerChosen) {
-
+      this.gameData.gameStatus.points += 10;
     }
+
     this.questionNumberCounter++;
     this.currentQuestion = this.gameQuestions$[this.questionNumberCounter - 1];
   }
 
   getChosenAnswer($event: string): void {
     this.answerChosen = $event;
+    this.options.forEach(option => !(this.answerChosen === option.cardText) ? option.selectedCard = false : option.selectedCard = true);
+  }
+
+  onSkip(): void {
+    // this.gameData.gameStatus.skips--;
+    this.gameService.decrementSkip();
+    this.questionNumberCounter++;
   }
 }
