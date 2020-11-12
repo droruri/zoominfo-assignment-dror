9bc553acfde2e6a1ff10e0730497bea03fa1ab6b
Guy Krief <guy.krief6@gmail.com>
2020-11-12 02:46:26 +0200

    renamed and added usage of store + handling state

--- src/app/questions/game.service.ts
+++ src/app/questions/game.service.ts
@@ -5,15 +5,19 @@ import {catchError, map} from 'rxjs/operators';
 import {Question} from './question';
 import {Store} from '@ngrx/store';
 import {CORRECT_HEADER, INCORRECT_HEADER, QUESTION_HEADER} from '../shared/global';
+import {GameData} from '../game/game-data';
+import {AppState} from '../app.state';
+import {DecrementSkip, LoadInitialData} from '../game/actions/game-data.actions';
 
 @Injectable({
   providedIn: 'root'
 })
-export class QuestionsService {
+export class GameService {
 
   private readonly url = 'https://opentdb.com/api.php?amount=10&type=multiple';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private store: Store<AppState>) {
   }
 
   public getQuestions(): Observable<Question[]> {
@@ -27,5 +31,17 @@ export class QuestionsService {
 
   private createQuestion(questionData: any): Question {
     return new Question(questionData[QUESTION_HEADER], questionData[CORRECT_HEADER], questionData[INCORRECT_HEADER]);
+  }
+
+  public getGameData(): Observable<GameData> {
+    return this.store.select('gameData');
+  }
+
+  public setStartGameData(game: GameData): void {
+    this.store.dispatch(new LoadInitialData(game));
+  }
+
+  public decrementSkip(): void {
+    this.store.dispatch(new DecrementSkip());
   }
 }
