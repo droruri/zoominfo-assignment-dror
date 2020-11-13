bb7c9ebf5eaed7e24a559f4c32a97dac381d3603
Guy Krief <guy.krief6@gmail.com>
2020-11-13 15:45:40 +0200

    added constants for remote and for leaderboard table headers

--- src/app/core/constants/global.ts
+++ src/app/core/constants/global.ts
@@ -1,4 +1,9 @@
 export const NUM_OF_QUESTIONS = 10;
+export const QUESTIONS_URL = 'https://opentdb.com/api.php?amount=10&type=multiple';
 export const QUESTION_HEADER = 'question';
 export const CORRECT_HEADER = 'correct_answer';
 export const INCORRECT_HEADER = 'incorrect_answers';
+export const RANKING_HEADER = 'Rank';
+export const USERNAME_HEADER = 'Username';
+export const POINTS_HEADER = 'Points';
+export const DATE_HEADER = 'Date played';
