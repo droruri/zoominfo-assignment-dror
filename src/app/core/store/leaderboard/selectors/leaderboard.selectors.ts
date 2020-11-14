9fd12a4392615b20f2a1549e9239972296b3d886
Guy Krief <guy.krief6@gmail.com>
2020-11-14 17:50:24 +0200

    using constant

--- src/app/core/store/leaderboard/selectors/leaderboard.selectors.ts
+++ src/app/core/store/leaderboard/selectors/leaderboard.selectors.ts
@@ -1,10 +1,11 @@
 import {LeaderboardState} from '../../app.state';
 import {createSelector} from '@ngrx/store';
 import {LeaderboardRecord} from '../../../models/leaderboard-record';
+import {ROWS_AT_LEADERBOARD} from '../../../constants/global';
 
 export const getLeaderboardState = (state: LeaderboardState) => state.leaderboard.leaderboardRecords;
 
 export const getBestRecordsState = (state: LeaderboardRecord[]) =>
-  state.sort((a, b) => (a.points - b.points)).slice(0, 10); // TODO: CONST
+  state.sort((a, b) => (a.points - b.points)).slice(0, ROWS_AT_LEADERBOARD);
 
 export const getBestRecords = createSelector(getLeaderboardState, getBestRecordsState);
