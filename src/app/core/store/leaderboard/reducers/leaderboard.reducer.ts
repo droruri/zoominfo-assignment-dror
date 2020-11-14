bdeffe5b183f4566ec154ed3a9acaa6183aae18a
Guy Krief <guy.krief6@gmail.com>
2020-11-14 18:23:20 +0200

    removing comments and unused import

--- src/app/core/store/leaderboard/reducers/leaderboard.reducer.ts
+++ src/app/core/store/leaderboard/reducers/leaderboard.reducer.ts
@@ -1,6 +1,5 @@
 import {Leaderboard} from '../../../models/leaderboard';
 import * as LeaderboardActions from '../actions/leaderboard.actions';
-import {LeaderboardRecord} from '../../../models/leaderboard-record';
 
 const initialState: Leaderboard = {
   leaderboardRecords: []
@@ -15,24 +14,3 @@ export function reducer(state: Leaderboard = initialState, action: LeaderboardAc
       return state;
   }
 }
-// TODO: is here?
-// function binaryInsertion(records: LeaderboardRecord[], recordToInsert: LeaderboardRecord, lowerBound: number, upperBound: number)
-//   : LeaderboardRecord[] {
-//   if (upperBound - lowerBound === 1) {
-//     if (recordToInsert < records[lowerBound]) {
-//       records.splice(lowerBound, 0, recordToInsert);
-//     }
-//     else if (recordToInsert > records[upperBound]) {
-//       records.splice(upperBound + 1, 0, recordToInsert);
-//     }
-//     else {
-//       records.splice(upperBound, 0, recordToInsert);
-//     }
-//   } else {
-//     const midPoint = Math.floor((upperBound - lowerBound) / 2) + lowerBound;
-//     recordToInsert < records[midPoint] ? binaryInsertion(records, recordToInsert, lowerBound, midPoint)
-//       : binaryInsertion(records, recordToInsert, midPoint, upperBound);
-//   }
-//
-//   return records;
-// }
