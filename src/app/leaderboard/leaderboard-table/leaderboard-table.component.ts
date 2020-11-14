6f659f451bd71f0df26a0f3daf294409ef021368
Guy Krief <guy.krief6@gmail.com>
2020-11-14 13:23:41 +0200

    table for displaying top records.

--- src/app/leaderboard/leaderboard-table/leaderboard-table.component.ts
+++ src/app/leaderboard/leaderboard-table/leaderboard-table.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {DATE_HEADER, POINTS_HEADER, RANKING_HEADER, USERNAME_HEADER} from '../../core/constants/global';
+import {LeaderboardService} from '../leaderboard.service';
+import {Observable} from 'rxjs';
+import {LeaderboardRecord} from '../../core/models/leaderboard-record';
 
 @Component({
   selector: 'app-leaderboard-table',
@@ -6,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leaderboard-table.component.css']
 })
 export class LeaderboardTableComponent implements OnInit {
+  readonly headerColumns = [RANKING_HEADER, USERNAME_HEADER, POINTS_HEADER, DATE_HEADER];
 
-  constructor() { }
+  constructor(private leaderboardService: LeaderboardService) { }
 
   ngOnInit(): void {
   }
 
+  getTopRecords(): Observable<LeaderboardRecord[]> {
+    return this.leaderboardService.getTopTenRecords();
+  }
 }
